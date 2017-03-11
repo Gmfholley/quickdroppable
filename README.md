5d68e94fa9d6da9b09129ec231b5ce7cfdb64a5e
Gmfholley <gmfholley@gmail.com>
2017-03-11 16:25:32 -0600

    First incomplete draft of instructions and samples

---
+++ README.md
@@ -0,0 +1,80 @@
+#Quickdroppable
+
+Quickdroppable is a jQuery plugin that offers basic drag & drop HTML5 functionality with support for touch/mobile.  `draggable` elements can be dragged and dropped by the user into `droppable` elements, where they will be appended/inserted.
+
+For a basic set up, use jQuery selectors to assign quickdroppable to your `draggable` and `droppable` html elements.  You must assign both `draggable` and `droppable` elements for Quickdroppable to work.
+
+```html
+<div id="draggable"></div>
+<div id="droppable"></div>
+
+<script>
+  (function(){
+  $(document).ready(function(){
+    $('#draggable').quickdroppable('draggable');
+    $('#droppable').quickdroppable('droppable');
+  });
+  })(jQuery);
+</script>
+```
+
+
+Quickdroppable has a wrapper around touch events that we will emit simulated mouse events, so the same drag/drop functionality will also work on mobile.
+
+Quickdroppable accepts the following parameters:
+
+```text
+type = 'draggable' or 'droppable'
+options = {
+	targetAssure: selector for target - might be parent of what was clicked,
+	dropCondition: selector for drop condition,
+	insertBefore: selector for insert of dragged into drop,
+	dragRemoveClass: class to add to drag/target element,
+	dragAddClass: class to remove from drag/target element,
+	dropRemoveClass: class to remove from drop element,
+	dropAddClass: class to add to drop element
+}
+callback = will be executed before drag/drop event finished
+```
+
+If you do not have any special options but you do have a callback, you must pass an empty object before the callback when you assign quickdroppable.
+
+```js
+(function(){
+	$(document).ready(function(){
+		$('#draggable').quickdroppable('draggable', {}, function(){
+			alert("I'm being dragged!");
+		});
+		$('#droppable').quickdroppable('droppable', {}, function(){
+			alert("I was dropped!");
+		});
+	});
+})(jQuery);
+```
+
+##Options for `draggable` elements
+`targetAssure`, `dragRemoveClass`, and `dragAddClass` are meant as options for `draggable` elements.
+
+ - `targetAssure` is the selector for the `draggable` item.  It ensures that when a user clicks on a child of of a `draggable` element, the `dragStart` event still fires.  Quickdroppable will look through 20 layers of children to try to find a matching selector to `targetAssure`.
+
+ ```html
+<div id="draggable">
+	<p>If a user clicks me instead of my parent, the draggable event will still fire because the targetAssure option has been set to bubble up until it finds `#draggable`.</p>
+</div>
+<div id="droppable"></div>
+
+<script>
+  (function(){
+  $(document).ready(function(){
+    $('#draggable').quickdroppable('draggable', {targetAssure: '#draggable'});
+    $('#droppable').quickdroppable('droppable');
+  });
+  })(jQuery);
+</script>
+ ```
+
+  - `dragRemoveClass` and `dragAddClass` will add and remove classes to the `draggable` element when a drag event is started.  The classes are triggered by 
+
+
+
+
---
+++ quickdroppable.js
@@ -0,0 +1,211 @@
+(function($){
+"use strict";
+/* *****************************
+* MIT license
+* developer: Wendy Holley (gmfholley)
+* allows html5 draga nd drop interface with touch support
+*				type = 'draggable' or 'droppable'
+*				options = {
+					targetAssure: selector for target - might be parent of what was clicked,
+					dropCondition: selector for drop condition,
+					insertBefore: selector for insert of dragged into drop,
+					dragRemoveClass: class to add to drag/target element,
+					dragAddClass: class to remove from drag/target element,
+					dropRemoveClass: class to remove from drop element,
+					dropAddClass: class to add to drop element
+				}
+				callback = will be executed before drag/drop event finished
+
+*/
+			// wrapper handler for Touch events to create drag events
+			var handleTouch = function(event){
+				event.preventDefault();
+				var type, dispatchTarget;
+				var touch = event.originalEvent.changedTouches[0];
+				switch(event.type){
+					case "touchstart": 
+						type="dragstart"; 
+						dispatchTarget = touch.target;
+						break;
+					case "touchmove": 
+						type="drag"; 
+						dispatchTarget = touch.target;
+						break;
+					case "touchend": 
+						var dropSite = document.elementFromPoint(touch.clientX, touch.clientY);
+						if (dropSite === event.target){
+							dispatchTarget = event.target;
+							type="click";
+						} else {
+							dispatchTarget = dropSite;
+							type="drop";
+						}
+						break;
+					default: return;
+				}
+
+				var simulatedMouse = document.createEvent("MouseEvent");
+				//event.initMouseEvent(type, canBubble, cancelable, view,
+    //                  detail (clicks), screenX, screenY, clientX, clientY,
+    //                  ctrlKey, altKey, shiftKey, metaKey,
+    //                  button (0: Main button pressed), relatedTarget);
+				simulatedMouse.initMouseEvent(type, true, true, window, 
+					1, touch.screenX, touch.screenY, touch.clientX, touch.clientY,
+					false, false, false, false, 0, null);
+				
+				dispatchTarget.dispatchEvent(simulatedMouse);
+
+			}
+
+
+			// return the target based on optional selector (might be parent of what was actually clicked)
+			var soughtTarget = function (initialTarget, selector){
+				var soughtTarget = initialTarget;
+
+				if (selector) {
+					var i = 0;
+					var firstTarget =  initialTarget;
+					while (! soughtTarget.is(selector) && i < 20) {
+						soughtTarget = soughtTarget.parent();
+						i++;
+					}
+					// if you could never find the selector, go with the original target
+					if (i === 20) { soughtTarget = initialTarget; }
+				}
+
+				return soughtTarget;
+			}
+
+			// assign a unique if the element doesn't already have one
+			var assureId = function(jElement) {
+				while (! jElement.attr('id')) {
+					var tryId = Math.random().toString(36);
+					if (! document.getElementById(tryId)){
+						jElement.attr('id', tryId);
+					}
+				}
+				return jElement;
+			}
+
+			// wrapper around jQuery or JavaScript implementation of setData to the event
+			var setDragId = function(dragId) {
+				if (event.dataTransfer){
+					event.dataTransfer.setData("dragId", dragId);
+				} else {
+					jQuery.Event.data = {"dragId": dragId};
+				}
+			}
+
+			// wrapper around jQuery or JavaScript implementation of setData to the event
+			var getDragId = function () {
+			  	if (event.dataTransfer){
+				    return event.dataTransfer.getData("dragId");
+			  	} else {
+				    return jQuery.Event.data["dragId"];
+			  	}
+			}
+
+			// defaultOptions
+
+			var def = {
+				dragAssure: '',
+				dropAssure: '',
+				dropCondition: '',
+				insertBefore: '',
+				dragRemoveClass: '',
+				dragAddClass: '',
+				dropRemoveClass: '',
+				dropAddClass: ''
+			}
+
+
+			$.fn.quickdroppable = function(type, options={}, callback){
+				
+				// don't overwrite def
+				var dropOptions = $.extend({}, def, options);
+
+
+				var dragStart = function(event) {
+
+
+
+				var dragTarget = soughtTarget($(event.target), dropOptions.dragAssure);
+				assureId(dragTarget);
+				
+				setDragId(dragTarget.attr('id'));
+
+				dragTarget.addClass(dropOptions.dragAddClass)
+						.removeClass(dropOptions.dragRemoveClass);
+
+				if (typeof callback === 'function') {
+					callback(event, dragTarget, dropOptions);
+				}
+			}
+
+			var dropEvent = function(event){
+				event.preventDefault();
+			  	event.stopPropagation();
+
+
+			  	var dropTarget = soughtTarget($(event.target), dropOptions.dropAssure);
+			  	
+			  	var data = getDragId();
+
+
+				var dragged = $('#' + data);
+
+				
+
+				if (typeof callback === 'function') {
+					callback(event, dropTarget, dragged, dropOptions);
+				}
+				//priority  = table headers
+				//only allow to drop if empty
+				if (!dropOptions.dropCondition || dropTarget.is(dropOptions.dropCondition)){
+					
+					if (dropOptions.insertBefore) {
+						dragged.insertBefore(dropOptions.insertBefore)
+					} else {
+						if (dropTarget.children().length > 0) {
+							dragged.insertBefore(dropTarget.children());						
+						} else {
+							dropTarget.append(dragged);
+						}
+					}
+
+					dropTarget.addClass(dropOptions.dropAddClass)
+						.removeClass(dropOptions.dropRemoveClass);
+
+
+
+					
+					dragged.addClass(dropOptions.dragAddClass)
+						.removeClass(dropOptions.dragRemoveClass);
+
+
+
+				}
+
+			}
+
+
+
+			if (type === 'droppable') {
+				this.on('drop', dropEvent)
+					.on('dragover', function(event){
+						event.preventDefault();
+						event.stopPropagation();
+					});
+			}
+
+			if (type === 'draggable') {
+				this.on('dragstart', dragStart)
+					.on('touchstart', handleTouch)
+					.on('touchmove', handleTouch)
+					.on('touchend', handleTouch)
+					.on('touchcancel', handleTouch);
+			}
+		}
+
+		
+})(jQuery);
